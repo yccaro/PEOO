7145126d6c80fd91ebf7cfd4e352e7b2f136f2c6
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-09-30 14:50:02 -0300

    finalizado o jogo da velha

--- tictactoe.py
+++ tictactoe.py
@@ -9,12 +9,8 @@ Exercises
 """
 
 from turtle import *
-
 from freegames import line
-
-gride = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+import random #importando biblioteca para jogadas aleatórias
 
 def grid():
     """Draw tic-tac-toe grid."""
@@ -23,29 +19,66 @@ def grid():
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
-
+#add nova cor e espessura 
 def drawx(x, y):
     """Draw X player."""
+    width(5)
+    color('red')
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
-
+#add nova cor e espessura 
 def drawo(x, y):
     """Draw O player."""
     up()
     goto(x + 67, y + 5)
     down()
+    width(5)
+    color('blue')
     circle(62)
 
 
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
+    #a primeira parte do cauculo identifica em qual casa o jogador teclou
+    #já a última parte dimenciona onde a casa fica no tabuleiro
 
+#função para gerar a jogada aleatória
+def jogadaAleatoria():
+    #cria uma lista para as casas vazias no gride
+    casaVazia = []
+
+    for i in range(3): #para cada uma das tres linhas
+        for j in range(3): #para cada uma das tres colunhas na linhas
+            if gride[i][j] == -1: # se estiver vazio
+                casaVazia.append((i, j)) # adiciona na lista das posições vazias
+
+    if casaVazia:
+        linha, coluna = random.choice(casaVazia)
+        x = linha * 133 - 200 #dimenciona pela posição escolhida onde fica no tabuleiro
+        y = coluna * 133 - 200 #dimenciona pela posição escolhida onde fica no tabuleiro
+        tap(x, y)
+
+#função para ver se ouve vitória
+def checarVitoria(player):
+    #para cada uma das tres linhas
+    for i in range(3):
+        #se todas as posiçoes da linha são do msm jogador ou todas as posiçoes da coluna são do mesmo jogador
+        if all(gride[i][j] == player for j in range(3)) or all(gride[j][i] == player for j in range(3)):
+            return True
+
+    #se todas as posiçoes da diagonal principal são do msm jogador ou todas as posiçoes da outra diagonal são do mesmo jogador
+    if all(gride[i][i] == player for i in range(3)) or all(gride[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
 
 state = {'player': 0}
 players = [drawx, drawo]
-
+gride = [[-1, -1, -1], 
+         [-1, -1, -1], 
+         [-1, -1, -1]]
 
 
 def tap(x, y):
@@ -53,90 +86,24 @@ def tap(x, y):
     x = floor(x)
     y = floor(y)
     player = state['player']
-    draw = players[player]
-    #add condicionais para casa preenchida
 
-    #coluna 1
-    if x == -200 and y == 66:
-        if (gride[0][0] == 0):
-            draw(x, y)
-            state['player'] = not player
-    
-    if x == -200 and y == -67:
-        if (gride[1][0] == 0):
-            draw(x, y)
-            state['player'] = not player
+    #identifica em qual casa o jogador teclou no gride
+    linha = int((x + 200) // 133) 
+    coluna = int((y + 200) // 133)
 
-    if x == -200 and y == -200:
-        if (gride[2][0] == 0):
-            draw(x, y)
-            state['player'] = not player
+    if gride[linha][coluna] == -1:
+        draw = players[player]
+        draw(x, y)
+        gride[linha][coluna] = player
+        update()
 
-    #coluna 2
-    if x == -67 and y == 66:
-        if (gride[0][1] == 0):
-            draw(x, y)
-            state['player'] = not player
-    
-    if x == -67 and y == -67:
-        if (gride[1][1] == 0):
-            draw(x, y)
-            state['player'] = not player
+        if checarVitoria(player):
+            print(f"Jogador {player + 1} venceu!")
 
-    if x == -67 and y == -200:
-        if (gride[2][1] == 0):
-            draw(x, y)
-            state['player'] = not player
-    
-    #coluna 3
-    if x == 66 and y == 66:
-        if (gride[0][2] == 0):
-            draw(x, y)
-            state['player'] = not player
-    
-    if x == 66 and y == -67:
-        if (gride[1][2] == 0):
-            draw(x, y)
-            state['player'] = not player
+        state['player'] = not player
 
-    if x == 66 and y == -200:
-        if (gride[2][2] == 0):
-            draw(x, y)
-            state['player'] = not player
-
-    update()
-    
-    # condicionais de preenchimento
-
-    #coluna 1
-    if x == -200 and y == 66:
-        gride[0][0] = 1
-    
-    if x == -200 and y == -67:
-        gride[1][0] = 1
-    
-    if x == -200 and y == -200:
-        gride[2][0] = 1
-
-    #coluna 2
-    if x == -67 and y == 66:
-        gride[0][1] = 1
-    
-    if x == -67 and y == -67:
-        gride[1][1] = 1
-    
-    if x == -67 and y == -200:
-        gride[2][1] = 1
-    
-    #coluna 3
-    if x == 66 and y == 66:
-        gride[0][2] = 1
-    
-    if x == 66 and y == -67:
-        gride[1][2] = 1
-    
-    if x == 66 and y == -200:
-        gride[2][2] = 1
+        if state['player'] == 1:
+            jogadaAleatoria()
 
 
 
